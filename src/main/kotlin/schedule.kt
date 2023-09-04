19953bd29029601ed285b3556b809568da9a206b
vir-air-vent <alberto.gruning.zen@icloud.com>
2023-09-04 15:33:23 -0300

    feat:when statements for first resolution o schedule exercise

--- src/main/kotlin/schedule.kt
+++ src/main/kotlin/schedule.kt
@@ -23,6 +23,22 @@ fun main(){
         else -> println("unspecified")
     }
 
+    var hour:Int
+    hour=20
+    println("time: $hour:00")
+    val isOpen = when (weekday) {
+        "Monday" -> hour in 8..12
+        "Tuesday" -> hour in 8..18
+        "Wednesday" -> hour in 8..18
+        "Thursday" -> hour in 8..18
+        "Friday" -> hour in 8..21
+        "Saturday" -> hour in 9..16
+        "Sunday" -> hour in 8..16
+        else -> false
+    }
 
+    print("Little Lemon is ")
+    if (isOpen) print("open") else print("closed")
+    println(" now.")
 
 }
