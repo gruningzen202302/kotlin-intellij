9d38053bfc0c742744b93a17687ff4b12bc03bef
vir-air-vent <alberto.gruning.zen@icloud.com>
2023-09-04 16:00:34 -0300

    feat:merge the 2 snippets to calculate the schedule once

--- src/main/kotlin/schedule.kt
+++ src/main/kotlin/schedule.kt
@@ -1,44 +1,61 @@
 fun main(){
-    println("Hello from IntelliJ")
     var weekday:String
-    // weekday = "Monday"
-    // weekday = "Tuesday"
-     weekday = "Wednesday"
-    // weekday = "Thursday"
-    // weekday = "Friday"
-    // weekday = "Saturday"
-    // weekday = "Sunday"
+    //weekday = "Monday"
+    //weekday = "Tuesday"
+    //weekday = "Wednesday"
+    weekday = "Thursday"
+    //weekday = "Friday"
+    //weekday = "Saturday"
+    //weekday = "Sunday"
 
     // weekday = "Holidays"
-
-    print("On $weekday, the opening hours are: ")
-    when(weekday){
-        "Monday" -> println("8 AM to 12 PM")
-        "Tuesday" -> println("8 AM to 18 PM")
-        "Wednesday" -> println("8 AM to 18 PM")
-        "Thursday" -> println("8 AM to 18 PM")
-        "Friday" -> println("8 AM to 21 PM")
-        "Saturday" -> println("9 AM to 16 PM")
-        "Sunday" -> println("8 AM to 16 PM")
-        else -> println("unspecified")
-    }
-
     var hour:Int
     hour=20
-    println("time: $hour:00")
-    val isOpen = when (weekday) {
-        "Monday" -> hour in 8..12
-        "Tuesday" -> hour in 8..18
-        "Wednesday" -> hour in 8..18
-        "Thursday" -> hour in 8..18
-        "Friday" -> hour in 8..21
-        "Saturday" -> hour in 9..16
-        "Sunday" -> hour in 8..16
-        else -> false
-    }
+    var range:IntRange
 
-    print("Little Lemon is ")
-    if (isOpen) print("open") else print("closed")
-    println(" now.")
+    print("On $weekday, the opening hours are: ")
+    val isOpen = when(weekday){
+        "Monday" -> {
+            range =8..18
+            println("${range.first} AM to ${range.last} PM")
+            hour in range
+        }
+        "Tuesday" -> {
+            range= 8..18
+            println("${range.first} AM to ${range.last} PM")
+            hour in range
+        }
+        "Wednesday" -> {
+            range = 8..18
+            println("${range.first} AM to ${range.last} PM")
+            hour in range
+        }
+        "Thursday" -> {
+            range= 8..18
+            println("${range.first} AM to ${range.last} PM")
+            hour in range
+        }
+        "Friday" -> {
+            range = 8..21
+            println("${range.first} AM to ${range.last} PM")
+            hour in range
+        }
+        "Saturday" -> {
+            range = 9..16
+            println("${range.first} AM to ${range.last} PM")
+            hour in range
+        }
+        "Sunday" -> {
+            range = 8..16
+            println("${range.first} AM to ${range.last} PM")
+            hour in range
+        }
+        else -> {
+            println("unspecified")
+            false
+        }
+    }
+    println("time: $hour:00")
+    println("Little Lemon is ${ if (isOpen) "open" else "closed"} now.")
 
 }
