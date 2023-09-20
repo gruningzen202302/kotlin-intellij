a9527bf6b779282f0e4bec2f32b148772eae1af1
vir-th-w11 <gruningzen202302@gmail.com>
2023-09-19 23:53:52 -0300

    feat:add single liner to print an upside-down triangle

--- src/main/kotlin/loops.kt
+++ src/main/kotlin/loops.kt
@@ -1,5 +1,8 @@
 fun main(){
     for(outer in 1..5){for(inner in 1..outer)print('*');println()}
+
     for(outer in 1..5){val limit=6-outer;for(inner in 1..limit)print('*');println()}
+
+    val tot=5;for(outer in 1..tot){val spaces=outer-1;for(space in 1..spaces)print(' ');val stars=tot+1-outer;for(star in 1..stars)print('*');println()}
 
 }
